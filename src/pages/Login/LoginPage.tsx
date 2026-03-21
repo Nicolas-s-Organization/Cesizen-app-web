3236b17eb02bd958bc891cdbad8e14b7e368e567
Nicolas Descarpentries <nicolas.descarp@outlook.fr>
2026-03-21 16:20:08 +0100

    feat: setup tailwind

--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -1,6 +1,11 @@
 const LoginPage = () => {
     return (
-        <div>Login</div>
+        <>
+            <div>Login</div>
+            <div className="bg-red-500 text-white p-4">
+                Tailwind fonctionne !
+            </div>
+        </>
     )
 }
 
